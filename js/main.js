acb3b64cc35b20663e63b05f17d7c4ca6a3accfd
mizuno <yuma.m0610@gmail.com>
2018-05-15 01:04:54 +0900

    文言追加

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@
 
   var panels = document.getElementsByClassName('panel');
   var spin = document.getElementById('spin');
+  var text = document.getElementById('index');
 
   var timers = [];
   var stopCount = 0;
 
   var cards = [
-    ['豊満な肢体の','ほとばしる情念の','吐出する情欲の','鮮やかな桃色の','粘膜同士の'],
+    ['豊満な肢体の','ほとばしる情念の','吐出する情欲の','鮮やかな桃色の','粘膜同士の','快楽の果ての','いやらしい匂いの','はち切れそうなワイシャツの',
+     '艶やかな'],
 
-    ['谷間からこぼれ落ちる','禁じられた','許されざる','アワビのような','蜜のような'],
+    ['谷間からこぼれ落ちる','禁じられた','許されざる','アワビのような','蜜のような','肉体を置き去りにする','悶絶する','倒錯する','グラマラスな',
+  '芳しい','ふしだらな','たわやかな','生暖かい'],
 
     ['確定拠出年金',
      '大殺界',
@@ -21,14 +24,19 @@
      '敷金・礼金',
      'コールオプション',
      '東証一部上場企業',
-      '眼精疲労',
-      '新卒一括採用']
+     '眼精疲労',
+     '新卒一括採用',
+     '一般性相対性理論',
+     'プライマリーバランス',
+     '尊王攘夷運動',
+     '排他的経済水域']
   ];
 
 
   function runSlot(n) {
     timers[n] = setTimeout(function(){
     panels[n].innerHTML = cards[n][Math.floor(Math.random() * cards[n].length)];
+    var element =
     runSlot(n);
   },20);
 }
@@ -42,6 +50,8 @@
         this.className = 'panel';
         if (stopCount === panels.length) {
           stopCount = 0;
+          text.className = '';
+          text.children[0].innerHTML = cards[2][2];
           // checkResults();
           spin.className = '';
         }
@@ -54,10 +64,11 @@
     runSlot(0);
     runSlot(1);
     runSlot(2);
-    this.className = 'inactive';
-    panels[0].className = "panel inactive"
-    panels[1].className = "panel inactive"
-    panels[2].className = "panel inactive"
+    this.className = 'hide';
+    text.className = 'hide';
+    panels[0].className = "panel"
+    panels[1].className = "panel"
+    panels[2].className = "panel"
 
   })();
 
